00ee6972a804030b1a649c101542b3cf90d586a8
Antony Jones <antony@beyonk.com>
2021-04-12 09:26:51 +0100

    add linting

--- src/FontTypes.js
+++ src/FontTypes.js
@@ -1,7 +1,7 @@
 module.exports = {
-	"eot": "embedded-opentype",
-	"woff2": "woff2",
-	"woff": "woff",
-	"ttf": "truetype",
-	"svg": "svg"
+  eot: 'embedded-opentype',
+  woff2: 'woff2',
+  woff: 'woff',
+  ttf: 'truetype',
+  svg: 'svg'
 }
